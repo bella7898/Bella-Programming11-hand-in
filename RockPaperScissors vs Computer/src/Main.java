50de360dd1c448ee1c440b1210b3531ee2a6f179
belw7898 <77554409+belw7898@users.noreply.github.com>
2021-11-03 18:41:28 -0700

    made changes to main.java for rock paper scissors

--- RockPaperScissors vs Computer/src/Main.java
+++ RockPaperScissors vs Computer/src/Main.java
@@ -4,86 +4,136 @@ public class Main {
     public static void main(String[] args){
         Scanner scan = new Scanner(System.in);
 
-        //first step: print out a string and get user input
+        //declare variables needed later
+        char x = 'x';
+        int choiceNum = 0;
+        int wins = 0;
+        int losses = 0;
 
-        System.out.println("Choose rock, paper, or scissors: r for 'rock', p for 'paper', and s for 'scissors");
-        String choice = (scan.next());
+        //set strings to make life easier
+        String lose = "You lost :(";
+        String win = "You won!";
+        String tie = "It's a tie.";
 
-        //get random integer with Math.random with range of 1 - 3
+        //print out the strings
+        System.out.print("Choose your weapon: r for 'rock', p for 'paper', and s for 'scissors'. To exit, type 'x'.");
+        System.out.print("\n************************************************************************************************\n" +
+                "Wins: " + Integer.toString(wins) + "; Losses: " + Integer.toString(losses) + " \nPlayer choice: \n");
 
-        int random = 1 + (int)(Math.random() * 3);
+        //get user input
+        char choice = scan.next().charAt(0);
 
-        //convert char variables to string
+        while(choice != 'x'){
 
-        char rock = 'r';
-        String rocks = String.valueOf(rock);
-        char paper = 'p';
-        String papers = String.valueOf(paper);
-        char scissor = 's';
-        String scissors = String.valueOf(scissor);
+            //get random number from 1 to 3
+            int random = 1 + (int)(Math.random() * 3);
 
-        /*
-        compare string variables declared above with player choice.
-        print out tie/win/lose statement.
-        if the player entered something else, it will print an error statement.
-        rock = 1, paper = 2, scissors = 3
-        rock < paper < scissors, but scissors < rock
-         */
 
-        if(choice.equals(rocks)){
-            int intChoice = 1;
-            if(random == intChoice) {
-                System.out.println("It's a tie.");
-                System.out.println("Computer chose: rock      Player chose: rock"); //tie statement
+            //assign letter r to 1, p to 2, and s to 3
+            if(choice == 'r'){
+                choiceNum = 1;
             }
-            else if(random == 3){
-                System.out.println("You won!");
-                System.out.println("Computer chose: scissors      Player chose: rock"); //win statement
+            else if(choice == 'p'){
+                choiceNum = 2;
             }
-            else{
-                System.out.println("You lost :(");
-                System.out.println("Computer chose: paper      Player chose: rock"); //loss statement
+            else if(choice == 's'){
+                choiceNum = 3;
             }
+            else if(choice == 'x'){ //check if the user wants to quit
+                break;
+            }
+            else{ //check for invalid input
+                System.out.print("Invalid input, please try again.");
+                break;
+            }
+
+
+            if(choiceNum == random) { //check for tie instance
+                System.out.print(tie);
+                if (choiceNum == 1) { //rock vs rock
+                    String var = "rock";
+                    System.out.print("\nComputer chose: " + var + "   Player chose: " + var);
+                } else if (choiceNum == 2) { //paper vs paper
+                    String var = "paper";
+                    System.out.print("\nComputer chose: " + var + "   Player chose: " + var);
+                } else { //scissors vs scissors
+                    String var = "scissors";
+                    System.out.print("\nComputer chose: " + var + "   Player chose: " + var);
+                }
+            }
+
+            else if(choiceNum == 3 && random == 1){ //scissors vs rock == lose
+                System.out.print(lose);
+                System.out.print("\nComputer chose: rock   Player chose: scissors");
+                losses++; // <----------------add increment to losses value
+            }
+
+            else if(choiceNum == 1 && random == 3){ //rock vs scissors == win
+                System.out.print(win);
+                System.out.print("\nComputer chose: scissors   Player chose: rock");
+                wins++; // <---------------add increment to wins value
+            }
+
+            else if(choiceNum > random){ //scissors vs paper or paper vs rock == win
+                System.out.print(win);
+                if(random == 1){
+                    String var = "rock";
+                    System.out.print("\nComputer chose: " + var);
+                }
+                else {
+                    String var = "paper";
+                    System.out.print("\nComputer chose: " + var);
+                }
+                if(choiceNum == 3){
+                    String var = "scissors";
+                    System.out.print("   Player chose: " + var);
+                }
+                else{
+                    String var = "paper";
+                    System.out.print("   Player chose: " + var);
+                }
+                wins++;
+            }
+
+            else{ //rock vs paper or paper vs scissors == lose
+                System.out.print(lose);
+                if(random == 3){
+                    String var = "scissors";
+                    System.out.print("\nComputer chose: " + var);
+                }
+                else if(random == 2){
+                    String var = "paper";
+                    System.out.print("\nComputer chose: " + var);
+                }
+                if(choiceNum == 1){
+                    String var = "rock";
+                    System.out.print("   Player chose: " + var);
+                }
+                else if(random == 3){
+                    String var = "paper";
+                    System.out.print("   Player chose " + var);
+                }
+                losses++;
+            }
+
+
+            //print next round's strings
+            System.out.print("\n************************************************************************************************\n" +
+                    "Wins: " + Integer.toString(wins) + "; Losses: " + Integer.toString(losses) + " \nPlayer choice: \n");
+            choice = scan.next().charAt(0); //scan for user input in next round
         }
-        else if(choice.equals(papers)){
-            int intChoice = 2;
-            if(random == intChoice) {
-                System.out.println("It's a tie.");
-                System.out.println("Computer chose: paper      Player chose: paper"); //paper ties with paper
-            }
-            else if(random == 1){
-                System.out.println("You won!");
-                System.out.println("Computer chose: rock      Player chose: paper"); //paper won against rock
-            }
-            else {
-                System.out.println("You lost :(");
-                System.out.println("Computer chose: scissors      Player chose: paper"); //paper lost against scissors
-            }
-        }
-        else if(choice.equals(scissors)){
-            int intChoice = 3;
-            if(random == intChoice) { //when random choice is equal to our choice, it's a tie.
-                System.out.println("It's a tie.");
-                System.out.println("Computer chose: scissors      Player chose: scissors");
-            }
-            else if(random == 2){ //when random is equal to 2, it's paper, which is defeated by our choice.
-                System.out.println("You won!");
-                System.out.println("Computer chose: paper      Player chose: scissors");
-            }
-            else { //else, which is when random is equal to 1, (rock) means we lost with our scissors.
-                System.out.println("You lost :(");
-                System.out.println("Computer chose: rock      Player chose: scissors");
-            }
-        }
-        else{
-            System.out.println("Invalid selection please play again.");  //error statement
+
+
+        //if loop has been broken (with user entering x) print statement
+        if(choice == 'x') {
+            System.out.println("Thanks for playing!");
         }
 
         /*
         Program by Bella W.
         Assignment for Computer Programming 11
         'Rock Paper Scissors'
-        2021-10-22
+        2021-11-03 (happy late halloween!)
         */
     }
 }
