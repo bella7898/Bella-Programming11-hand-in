04103f7c6499647c57f73b80b1636997294da1c3
belw7898 <77554409+belw7898@users.noreply.github.com>
2021-12-01 22:29:07 -0800

    cleaned up some unnecessary variables

--- ArrayAssignment/src/Main.java
+++ ArrayAssignment/src/Main.java
@@ -1,70 +1,67 @@
 import java.util.Arrays;
 
 public class Main {
-/*
+
     private static int[] addTo(int[] a, int newData) {
         //new method called addTo with parameters 'a' as an int array and the int newData.
-        int i = 0;
+
         int x;
         //declare int 'i' and 'x'
+
         int[] newAr = new int[a.length + 1];
         //create a new int array 'newAr' set with one more index position than 'a'
+
         newAr[a.length] = newData;
         //add the new data to the last index of newAr
-            //finally, copy everything else from a to newAr.
-            while(i < a.length){
-                x = a[i];
-                // x is equal to the int in 'a' at the index 'i'
-                newAr[i] = x;
+            
+            for(int i = 0; i < a.length;i++){
+                a[i] = newAr[i];
                 //the index 'i' in newAr is now set to equal x
-                i++;
-                //add 1 to i each time the while loop repeats.
             }return newAr;
+            //returns newAr with all data copied from a
+            //and newData added to the last index position
         }
 
 
     private static int[] delFrom(int[] a, int rmvData) {
         //new method called delFrom with parameters 'a' as an int array and the int rmvData.
-        int i = 0;
+
         int x;
-        //declare int 'i' and 'x'
+        //declare int 'i'
+
         int[] newAr = new int[a.length - 1];
         //create a new int array 'newAr' set with one less index position than 'a'
-        //finally, copy everything else from 'a' to newAr (except for the last num)
-        while(i < a.length - 1){
-            x = a[i];
-            // x is equal to the int in 'a' at the index 'i'
-            newAr[i] = x;
-            //the index 'i' in newAr is now set to equal x
-            i++;
-            //add 1 to i each time the while loop repeats.
+
+        for(int i =0; i < a.length - 1; i++){
+            a[i] = newAr[i];
+            //copies all the data from 'a' to 'newAr'
+            //except for the last num 
         }return newAr;
     }
-*/
 
     private static int[] insertInt(int[] a, int insertNum, int indexNum) {
         //new method called insertIn with parameters 'a', insertNum and the int indexNum.
-        //declare variables
+        
         int x;
-        //declare new array 'newAr' with 1 more index position than 'a'
+        //declare needed variables
+
         int y = a.length + 1;
         int[] newAr = new int[y];
+        //declare new array 'newAr' with 1 more index position than 'a'
 
+        //declare int to equal 0
+        //loop while i is smaller than y (a.length + 1) 
+        //increment to i each loop
         for(int i = 0; i < y; i++) {
-            if (i < indexNum) {
-                x = a[i];
-                newAr[i] = x;
-                System.out.println(i);
-                System.out.println(x);
-                System.out.println(newAr[i]);
-            } else if (i == indexNum) {
-                newAr[i] = insertNum;
-                System.out.println(i);
-                System.out.println(newAr[i]);
-            } else { //(if i > indexNum)
+
+            if (i < indexNum) { //when i is 0 or 1 in my test case
+                a[i] = newAr[i]; //simply copy from a to newAr
+
+            } else if (i == indexNum) { //i is 2 in test
+                newAr[i] = insertNum; //will insert num in index position 2
+
+            } else { //if i > indexNum
                 newAr[i] = a[i-1];
-                System.out.println(i);
-                System.out.println(newAr[i]);
             }
         }
         return newAr;
@@ -72,7 +69,7 @@ public class Main {
 
     public static void main(String[] args) {
         int[] a = {5, 4, 3, 2, 1}; //5 index positions in length [0, 1, 2, 3, 4]
-        /*
+        
         int newData = 0;
         int[] array1 = addTo(a, newData);
         while(i < array1.length){
@@ -81,7 +78,7 @@ public class Main {
             i++;
         }
 
-        int rmvData = 1;
+        int rmvData = 2;
 
         int[] array2 = delFrom(a, rmvData);
         while (i < array2.length) {
@@ -89,7 +86,7 @@ public class Main {
             System.out.println(x);
             i++;
         }
-*/
+
         int insertNum = 0;
         int indexNum = 2;
 
